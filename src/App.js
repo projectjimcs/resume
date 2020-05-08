1360111fedaed5d254f8c78219b7dc0f930b9954
Jim <projectjimcs@gmail.com>
2020-05-07 22:25:59 -0700

    Deleted sample files and put initial skeleton for page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className='main-header'>
+          Resume
+        </div>
+        <div className='main-content'>
+
+          <div className='left-side'>
+            <div className='experience-block'>
+              <span>Developer Experience</span>
+              <div className='skill-blocks-container'>
+                <div className='skill-block'>
+                  Javascript
+                </div>
+                <div className='skill-block'>
+                  PHP
+                </div>
+                <div>
+                  React
+                </div>
+              </div>
+            </div>
+            <div className='experience-block'>
+              Other Experience
+            </div>
+            <div className='experience-block'>
+              Education
+            </div>
+          </div>
+
+          <div className='right-side'>
+            Right Side
+          </div>
+
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
