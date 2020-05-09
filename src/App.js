2e24730e4f78b884f50d991d5c73de699d191cbf
Jim <projectjimcs@gmail.com>
2020-05-08 18:03:42 -0700

    Built out rows for dev experience

--- src/App.js
+++ src/App.js
@@ -23,17 +23,50 @@ export default class App extends React.Component {
             <div className='experience-block'>
               <span className='sub-heading'>Developer Experience</span>
               <div className='skill-blocks-container'>
-                <div className='skill-block'>
-                  Javascript
+
+                <div className='skill-row'>
+                  <div className='skill-block'>
+                    PHP
+                  </div>
+                  <div className='skill-block'>
+                    Javascript
+                  </div>
+                  <div className='skill-block'>
+                    C#
+                  </div>
+                  <div className='skill-block'>
+                    Ruby
+                  </div>
                 </div>
-                <div className='skill-block'>
-                  PHP
+
+                <div className='skill-row'>
+                  <div className='skill-block'>
+                    Express
+                  </div>
+                  <div className='skill-block'>
+                    Laravel
+                  </div>
+                  <div className='skill-block'>
+                    CodeIgniter
+                  </div>
+                  <div className='skill-block'>
+                    Unity
+                  </div>
                 </div>
-                <div className='skill-block'>
-                  React
-                </div>
-                <div className='skill-block'>
-                  NodeJs
+
+                <div className='skill-row'>
+                  <div className='skill-block'>
+                    HTML
+                  </div>
+                  <div className='skill-block'>
+                    CSS
+                  </div>
+                  <div className='skill-block'>
+                    JQuery
+                  </div>
+                  <div className='skill-block'>
+                    React
+                  </div>
                 </div>
               </div>
             </div>
