6e8580ada219028a2f49d866a16c39c31c443724
Jim <projectjimcs@gmail.com>
2020-05-14 16:02:51 -0700

    Changed pixel

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.mobileSize = 376;
+    this.mobileSize = 726;
 
     this.state = {
       currentExpression: 'default',
